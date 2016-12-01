1a0e8213936d7ff744372df7dc1941d230f555a3
jokerno1 <wbeing@gmail.com>
2016-12-01 21:33:46 +1300

    Add files via upload

---
+++ Chapter 2 - Program 4/Chapter 2 - Program 4/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter_2___Program_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void changeText_Click(object sender, EventArgs e)
+        {
+            if (enableCheckbox.Checked == true)
+            {
+                if (lableToChange.Text == "Right")
+                {
+                    lableToChange.Text = "Left";
+                    lableToChange.TextAlign = ContentAlignment.MiddleLeft;
+                }
+                else
+                {
+                    lableToChange.Text = "Right";
+                    lableToChange.TextAlign = ContentAlignment.MiddleRight;
+                }
+            }
+            else
+            {
+                lableToChange.Text = "Text changing is disabled";
+                lableToChange.TextAlign = ContentAlignment.MiddleCenter;
+            }
+        }
+    }
+}
